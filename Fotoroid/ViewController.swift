b722a4ee72d11e0931845e64ea98bdb351e876e9
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-09-23 20:01:17 -0300

    Initial Commit

---
+++ Fotoroid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fotoroid
+//
+//  Created by Vinícius Tinajero Salomão on 23/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
